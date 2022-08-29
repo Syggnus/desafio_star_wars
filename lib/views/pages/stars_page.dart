02e86f00f9a3629242cdd6098d9d78778f6bd72a
kevinLyf <chuan.ferreira@outlook.com>
2022-08-28 23:14:04 -0300

    Criação da Page de Favoritos

---
+++ lib/views/pages/stars_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class StarsPage extends StatefulWidget {
+  const StarsPage({Key? key}) : super(key: key);
+
+  @override
+  State<StarsPage> createState() => _StarsPageState();
+}
+
+class _StarsPageState extends State<StarsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
