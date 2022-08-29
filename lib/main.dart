918584acdcc2dcc3fe4ac4c76bdb7a06b82556c9
kevinLyf <chuan.ferreira@outlook.com>
2022-08-28 23:08:33 -0300

    Update main.dart
    Limpando o main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,6 @@
-import 'package:desafio_star_wars/pages/home_page.dart';
 import 'package:flutter/material.dart';
+import 'app.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatefulWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: const HomePage(),
-        theme: ThemeData(
-          brightness: Brightness.light,
-        ),
-        darkTheme: ThemeData(
-          brightness: Brightness.dark,
-        ),
-        themeMode: ThemeMode.dark);
-  }
+void main() async {
+  runApp(const StarWarsApp());
 }
