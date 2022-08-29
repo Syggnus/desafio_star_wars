c6d1703f4b855934f011864d61238500e13aacb5
kevinLyf <chuan.ferreira@outlook.com>
2022-08-28 23:13:29 -0300

    Page Config Avatar
    Página para configuração do Avatar

---
+++ lib/views/pages/config_avatar_page.dart
@@ -0,0 +1,91 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+import 'package:fluttermoji/fluttermoji.dart';
+import 'home_page.dart';
+import 'in_app_browser_page.dart';
+
+class ConfigAvatarPage extends StatelessWidget {
+  const ConfigAvatarPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var width = MediaQuery.of(context).size.width;
+    return Scaffold(
+      appBar: AppBar(
+        leadingWidth: 100,
+        leading: TextButton(
+          child: const Text(
+            "Site Oficial",
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const InAppBrowserPage(),
+              ),
+            );
+          },
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const HomePage(),
+                ),
+              );
+            },
+            icon: FluttermojiCircleAvatar(),
+          )
+        ],
+        backgroundColor: Colors.black,
+      ),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 30),
+                child: FluttermojiCircleAvatar(
+                  radius: 100,
+                  backgroundColor: Colors.grey[200],
+                ),
+              ),
+              SizedBox(
+                width: min(600, width * 0.85),
+                child: Row(
+                  children: [
+                    Text(
+                      "Customize:",
+                      style: Theme.of(context).textTheme.headline6,
+                    ),
+                    const Spacer(),
+                    FluttermojiSaveWidget(),
+                  ],
+                ),
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 8.0, vertical: 30),
+                child: FluttermojiCustomizer(
+                  scaffoldWidth: min(600, width * 0.85),
+                  autosave: false,
+                  theme: FluttermojiThemeData(
+                    boxDecoration: const BoxDecoration(
+                      boxShadow: [BoxShadow()],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
