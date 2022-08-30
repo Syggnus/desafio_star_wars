5f8ffc38213f400e8898a76f52d8c07c6d5b9900
kevinLyf <chuan.ferreira@outlook.com>
2022-08-29 21:23:46 -0300

    Refatoração (Substituição => API Controller)

--- lib/controllers/films_api.dart
+++
@@ -1,9 +0,0 @@
-import 'package:http/http.dart' as http;
-
-const url = "https://swapi.dev/api/films/";
-
-class FilmsApi {
-  static Future getFilms() async {
-    return await http.get(Uri.parse(url));
-  }
-}
